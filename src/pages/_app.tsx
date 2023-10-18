e932d32257e0c0bffb8f50b1c2758e619ec8a7d0
FreeJ1nG <Andrewjeremy12345@gmail.com>
2023-10-18 18:22:14 +0700

    fix: add provider wrappers for components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
+import { Provider as ReduxProvider } from 'react-redux';
 import type { AppProps } from 'next/app';
+
+import { AuthContextProvider } from '@/common/contexts/authContext';
+import { ToasterContextProvider } from '@/common/contexts/toasterContext';
+import ThemeProvider from '@/modules/mui/theme';
+import store from '@/modules/redux/store';
 
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <ReduxProvider store={store}>
+        <ToasterContextProvider>
+          <AuthContextProvider>
+            <Component {...pageProps} />
+          </AuthContextProvider>
+        </ToasterContextProvider>
+      </ReduxProvider>
+    </ThemeProvider>
+  );
 }
